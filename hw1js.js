787b0334a47ad283925d712c54e2edad3d11cfc6
Mamanbule <dima.kovalchukqa@gmail.com>
2022-05-10 23:00:21 +0300

    HW JS

---
+++ hw1js.js
@@ -0,0 +1,31 @@
+    let age_1 = 2
+
+    if (age_1 >=18 && age_1 <60 ) {
+        console.log("Full content")
+    }
+    else if (age_1 < 18) {
+        console.log("Multiki")
+    }
+    else if (age_1 >= 60) {
+        console.log("Kultura")
+    }
+    else {
+        console.log("good luck")
+    }
+
+let age ="1"
+
+if (typeof(age) =="number"){
+    if (age >=18 && age <60 ) {
+        console.log("Full content")
+    }
+    else if (age < 18) {
+        console.log("Multiki")
+    }
+    else if (age >= 60) {
+        console.log("Kultura")
+    }
+} else {
+    console.log("good luck")
+}
+    
